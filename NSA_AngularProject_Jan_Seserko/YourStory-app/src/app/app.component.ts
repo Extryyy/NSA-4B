d6035aab5cc8488a4a1976ebceae85ff9f272ab6
Extryyy <seserko.jan@gmail.com>
2024-10-05 23:41:03 +0200

    Koren NSA 2.1

--- NSA_AngularProject_Jan_Seserko/YourStory-app/src/app/app.component.ts
+++ NSA_AngularProject_Jan_Seserko/YourStory-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],  // Add FontAwesomeModule to imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'YourStory-app';
+
+  constructor(library: FaIconLibrary) {
+    // Add the solid icons to the library
+    library.addIconPacks(fas);
+  }
 }
